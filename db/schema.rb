50e1aac0da245a6d2cb1ab32d861f2099fba8473
Diane <d.bunod@gmail.com>
2018-12-12 22:17:36 -0800

    create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_070441) do
+ActiveRecord::Schema.define(version: 2018_12_13_055628) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2018_12_11_070441) do
   end
 
   create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
